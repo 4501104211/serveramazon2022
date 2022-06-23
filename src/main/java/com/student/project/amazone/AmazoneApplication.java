06bf69d16af26dea263214442735373a0471ef32
nguyencaothai <lanhoxga@gmail.com>
2022-06-23 17:56:23 +0700

    deploy

--- src/main/java/com/student/project/amazone/AmazoneApplication.java
+++ src/main/java/com/student/project/amazone/AmazoneApplication.java
@@ -4,8 +4,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+
 
 @SpringBootApplication
+@ComponentScan({ "com.student.project.amazone.*"} )
 public class AmazoneApplication  {
 
     public static void main(String[] args) {

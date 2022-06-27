56e2f4581686db1554fd5f39d6d6892166cfd494
nguyencaothai <lanhoxga@gmail.com>
2022-06-28 00:22:50 +0700

    fix bug deploy

--- src/main/java/com/student/project/amazone/entity/DateAbstract.java
+++ src/main/java/com/student/project/amazone/entity/DateAbstract.java
@@ -2,46 +2,36 @@ package com.student.project.amazone.entity;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.Data;
-import org.joda.time.DateTime;
-import org.joda.time.DateTimeZone;
 
 import javax.persistence.*;
 import java.io.Serializable;
-import java.time.Instant;
-import java.time.ZoneId;
-import java.time.ZonedDateTime;
 import java.util.Calendar;
 import java.util.Date;
 
 @Data
 @MappedSuperclass
 public abstract class DateAbstract implements Serializable {
-    @JsonFormat(pattern="yyyy-MM-dd ")
+    private static final String MY_TIME_ZONE="Asia/Ho_Chi_Minh";
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern="yyyy-MM-dd HH:mm:ss",timezone = MY_TIME_ZONE)
     @Temporal(value = TemporalType.TIMESTAMP)
     @Column(name = "last_updated")
-    private ZonedDateTime LastUpdated;
+    private Date LastUpdated;
 
-    @JsonFormat(pattern="yyyy-MM-dd ")
+    @JsonFormat(shape = JsonFormat.Shape.STRING,pattern="yyyy-MM-dd HH:mm:ss",timezone = MY_TIME_ZONE)
     @Temporal(value = TemporalType.TIMESTAMP)
     @Column(name = "create_at")
-    private ZonedDateTime CreateAt;
-
+    private Date CreateAt;
 
     @PrePersist
     public void prePersist() {
         if (CreateAt == null) {
-            Instant nowUtc = Instant.now();
-            ZoneId asiaSingapore = ZoneId.of("Asia/Ho_Chi_Minh");
-            ZonedDateTime nowAsiaSingapore = ZonedDateTime.ofInstant(nowUtc, asiaSingapore);
-            this.CreateAt  =  nowAsiaSingapore;
+            this.CreateAt  =  new Date();
         }
     }
 
     @PreUpdate
     public void preUpdate() {
-        Instant nowUtc = Instant.now();
-        ZoneId asiaSingapore = ZoneId.of("Asia/Ho_Chi_Minh");
-        ZonedDateTime nowAsiaSingapore = ZonedDateTime.ofInstant(nowUtc, asiaSingapore);
-        this.LastUpdated  =  nowAsiaSingapore;
+        Date date = new Date();;
+        this.LastUpdated  =  new Date();
     }
 }
